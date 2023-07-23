a9f6121da3dd0f7a2a130347ebed8d489dd69419
Amadou Barry <barryamadou191@gmail.com>
2023-07-23 22:05:38 +0000

    Bugs corrections

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -2,10 +2,78 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Customer;
+use App\Models\Folder;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Date;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public array $stats = [], $years = [], $total = [];
+    public $stat , $year, $month, $week, $yearweeek, $date;
+
+    public array $colors = [], $months = [];
+
+    public bool $firstRun = true, $showDataLabels = true;
+
+    public $folders = [], $products = [], $customers = [], $containers = [], $breakdowns = [];
+
+    protected $listeners = [
+        'onPointClick' => 'handleOnPointClick',
+        'onSliceClick' => 'handleOnSliceClick',
+        'onColumnClick' => 'handleOnColumnClick',
+    ];
+
+    public function mount()
+    {
+        $this->colors = [
+            '#64748b', '#364152','#1a202e', // cool-gray
+            '#f05252', '#c81e1e', '#771d1d', // red
+            '#ff5a1f', '#b43403', '#771d1d', // orange
+            '#c27803', '#8e4b10', '#633112', // yellow
+            '#0e9f6e', '#046c4e', '#014737', // green
+            '#0694a2', '#036672', '#014451', // teal
+            '#3f83f8', '#1a56db', '#233876', // blue
+            '#6875f5', '#5145cd', '#362f78', // indigo
+            '#9061f9', '#6c2bd9', '#4a1d96', // purple
+            '#e74694', '#bf125d', '#751a3d', // pink
+        ];
+
+        $this->months = [
+            'January', 'February', 'March', 'April', 'May', 'June', 'July',
+            'August', 'September', 'October', 'November', 'December'
+        ];
+
+        $this->stats = [
+            'productsSalesCount' => 'Les matériels les plus vendus',
+            'bikesSalesCount' => 'Les types de motos les plus vendus',
+            'bikesReparationsCount' => 'Les types de motos les plus reparés',
+            'repairBreakdownsCount' => 'Les pannes les plus fréquentes',
+            'repairmansReparationsCount' => 'Les reparateurs qui ont plus reparer',
+            'customersSalesSum' => 'Les meilleurs clients'
+        ];
+
+        // Années disponibles
+        array_push($this->years, 'Toutes les années');
+        $this->year = now()->format('Y');
+
+        $this->folders = Folder::query()->get();
+
+        $firstFolder = $this->folders->first();
+        if ($firstFolder)
+            $this->years = range(Date::create($firstFolder->created_at)->format('Y'), (int)$this->year);
+
+        //if(Auth::user()->isAdmin()) {
+            $this->total['folders'] = $this->folders->count();
+            $this->total['pending_folders'] = $this->folders->where('status', 'En attente')->count();
+            $this->total['process_folders'] = $this->folders->where('status', 'En cours')->count();
+            $this->total['closed_folders'] = $this->folders->where('status', 'Fermé')->count();
+            $this->total['customers'] = Customer::query()->count();
+        //}
+    }
+
     public function render()
     {
         return view('dashboard');
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,24 @@
 @section('title', 'Tableau de bord')
 
 <div>
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <x-small-box type="warning" icon="fas fa-folder-plus" text="Dossiers en attente"
+                         :number="$total['pending_folders'] ?? 0" route="pending-folders"></x-small-box>
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-small-box type="primary" icon="fas fa-folder-open" text="Dossiers en cours"
+                         :number="$total['process_folders'] ?? 0" route="progress-folders"></x-small-box>
+        </div>
+        <div class="col-lg-3 col-6">
+            <x-small-box type="success" icon="fas fa-folder-closed" text="Dossiers fermés"
+                         :number="$total['closed_folders'] ?? 0" route="closed-folders"></x-small-box>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <x-small-box type="info" icon="fas fa-users" text="Total clients"
+                         :number="$total['customers'] ?? 0" route="customers"></x-small-box>
+        </div>
+    </div>
 
 </div>
